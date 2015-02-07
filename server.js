607dae0227ce704c32420b6ea672108a83401d73
Shayaan Siddiqui <shayaan.siddiqui@mac.com>
2015-02-07 16:36:43 -0500

    Updated html to work with angular (main.js) and tested responsive design

--- server.js
+++ server.js
@@ -2,9 +2,14 @@
 var express = require('express');
 var app = express();
 
+
+app.use('/public', express.static(__dirname + '/public'));
+
 app.get("/", function(req, res){
 	res.sendFile(process.cwd() + "/public/index.html");
 })
+
+
 
 app.listen(8080);
 console.log("App listening to port 8080");
