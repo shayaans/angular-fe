bedfdb76d1e8183cd8c95d0ebf6fb145c1422730
Shayaan Siddiqui <shayaan.siddiqui@mac.com>
2015-02-07 16:10:51 -0500

    Added HTML and updated references to appropiate CSS files

--- server.js
+++ server.js
@@ -2,5 +2,9 @@
 var express = require('express');
 var app = express();
 
+app.get("/", function(req, res){
+	res.sendFile(process.cwd() + "/public/index.html");
+})
+
 app.listen(8080);
 console.log("App listening to port 8080");
