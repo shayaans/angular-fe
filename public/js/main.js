e609d2c4bd369fa9d0a9d0059c862f45b86df346
Shayaan Siddiqui <shayaan.siddiqui@mac.com>
2015-02-08 12:05:06 -0500

    Working Code and CSS. Added support for route to external json data so we can scale.

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,7 @@
 angular.module('displayImageGrid', []);
 
-angular.module("displayImageGrid").controller("mainController", function($scope) {
-  $scope.results = [{
-    "Id": 1,
-    "IMI": {
-      "Name": "Fly Round-Trip to Myrtle Beach Philadelphia",
-      "Brand": "Expedia",
-      "Image": "https://stream.inmarkit.com/uploads/mm/P498/62/GwVmHPI_50x50.jpg"
-    }
-  }, {
-    "Id": 1,
-    "IMI": {
-      "Name": "Fly Round-Trip to Myrtle Beach Philadelphia",
-      "Brand": "Expedia",
-      "Image": "https://stream.inmarkit.com/uploads/mm/P498/62/GwVmHPI_50x50.jpg"
-    }
-  }, {
-    "Id": 1,
-    "IMI": {
-      "Name": "Fly Round-Trip to Myrtle Beach Philadelphia",
-      "Brand": "Expedia",
-      "Image": "https://stream.inmarkit.com/uploads/mm/P498/62/GwVmHPI_50x50.jpg"
-    }
-  }];
+angular.module('displayImageGrid').controller('mainController', function($scope, $http) {
+  $http.get('/results.json').then(function(response) {
+    $scope.results = response.data.marks;
+  });
 });
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 
 var express = require('express');
+var fs = require('fs');
 var app = express();
-
 
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get("/", function(req, res){
 	res.sendFile(process.cwd() + "/public/index.html");
-})
+});
 
-
+app.get("/results.json", function(req, res) {
+  fs.readFile(process.cwd() + '/data/results.json', 'utf8', function(err, data) {
+    res.json(JSON.parse(data));
+  });
+});
 
 app.listen(8080);
 console.log("App listening to port 8080");
